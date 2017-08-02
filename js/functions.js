729d886f31f48608f5d060802a64b61f34689d81
Danny <sdanielabelvedere@gmail.com>
2017-08-02 19:41:36 -0300

    setup materialize  directory

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	//Initialize select elements
+    $('select').material_select();
+    //Activate dropdown
+    $(".dropdown-button").dropdown();
+
+  }); //End Document Ready
