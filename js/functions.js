e6c496c880e281be3199c7fde7640a351af6527b
Danny <sdanielabelvedere@gmail.com>
2017-08-03 00:34:14 -0300

    Input type range fixed with hack

--- js/functions.js
+++ js/functions.js
@@ -4,5 +4,14 @@ $(document).ready(function() {
     $('select').material_select();
     //Activate dropdown
     $(".dropdown-button").dropdown();
+   $('input[type="range"]').on("input change", function () {
+	    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
+	    console.log(val);
+	    //it duplicates the input type range width property
+	    var val2 = val*414;
+	    console.log(val2);
+
+	    $('.hack').css('width', val2); 
+	});//end type=range
 
   }); //End Document Ready
