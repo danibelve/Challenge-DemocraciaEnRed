6ce76bbf6a2e83572fbe8c04336b9002a4261fa7
Danny <sdanielabelvedere@gmail.com>
2017-08-03 01:11:27 -0300

    using variables to get input type range width

--- js/functions.js
+++ js/functions.js
@@ -6,10 +6,13 @@ $(document).ready(function() {
     $(".dropdown-button").dropdown();
    $('input[type="range"]').on("input change", function () {
 	    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
-	    console.log(val);
+	    //console.log(val);
+	    var width = $(this).css('width');
+	    var num = parseInt(width);
+	    //console.log("num " + num);
 	    //it duplicates the input type range width property
-	    var val2 = val*453;
-	    console.log(val2);
+	    var val2 = val*num;
+	    //console.log(val2);
 
 	    $('.hack').css('width', val2); 
 	});//end type=range
