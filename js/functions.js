8b20b06de2dffb38736b10229494f7fb0247106e
Danny <sdanielabelvedere@gmail.com>
2017-08-03 20:50:45 -0300

    validate name

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     $('select').material_select();
     //Activate dropdown
     $(".dropdown-button").dropdown();
-    //Range function 
+
+ //Range function 
    $('input[type="range"]').on("input change", function () {
 	    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
 	    //console.log(val);
@@ -16,7 +17,6 @@ $(document).ready(function() {
 	    $('.hack').css('width', val2); 
 	});//end type=range
 
-<<<<<<< HEAD
   }); //End Document Ready
 
 //This wont work in Chrome for security reasons
@@ -27,42 +27,13 @@ $.getJSON('js/document.json', function(data) {
 		$('#color').append('<option value="' + fur[index].value +'">'+ fur[index].label +'</option>');
 	});
 }); // close getJSON()
-/*
-$.ajax({
-  url: 'http://myjson.com/17cfa1',
-  dataType: 'jsonp',
-  success: function (data) {
-    var fur = data.fur_color;
-	$.each(fur, function (index){
-			$('#color').append('<option value="' + fur[index].value +'">'+ fur[index].label +'</option>');
-		});
-	}); // close getJSON()
-   error: function(jqXHR, textStatus, errorThrown) {
-     //Error handling code
-     alert('Oops there was an error');
-   }
-  }
-});*/
-=======
-}); //End Document Ready
-$.getJSON('js/document.json', function(data) {
-	var fur = data.fur_color;
-	$.each(fur, function (index){
-        $('#color').append('<option value="' + fur[index].value +'">'+ fur[index].label +'</option>');
-	});
-}); // close getJSON()
-								    
+
 function validate(){
 		var name = document.getElementById('name').value;
-		var gender = document.getElementById('gender').value;
-		var furColor = document.getElementById('fur').value;
-		var mail = document.getElementById('email').value;
-		check = document.getElementById('checkbox').value; 
 		console.log(name);
 		if(name == '') {
-			$('#name-error').removeClass("hide");
+			$('#name-error').css("display","block");
 		}
 
 };//end function
 
->>>>>>> aeb9c2c7b81c1cf046a5b5ff890edf5a4ed5f416
