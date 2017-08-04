4d59c6abc2a659a60c92fe54414ec94ca7deee6c
Danny <sdanielabelvedere@gmail.com>
2017-08-03 21:55:25 -0300

    Trying to use materialize data-error

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $('select').material_select();
     //Activate dropdown
     $(".dropdown-button").dropdown();
-
  //Range function 
    $('input[type="range"]').on("input change", function () {
 	    var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
@@ -16,7 +15,6 @@ $(document).ready(function() {
 	    //console.log(val2);
 	    $('.hack').css('width', val2); 
 	});//end type=range
-
   }); //End Document Ready
 
 //This wont work in Chrome for security reasons
@@ -28,12 +26,13 @@ $.getJSON('js/document.json', function(data) {
 	});
 }); // close getJSON()
 
-function validate(){
+
+/*function validate(){
 		var name = document.getElementById('name').value;
 		console.log(name);
 		if(name == '') {
 			$('#name-error').css("display","block");
+			$('html, body').animate({
+			        scrollTop: $('#name').offset().top}, 500)
 		}
-
-};//end function
-
+};//end function*/
