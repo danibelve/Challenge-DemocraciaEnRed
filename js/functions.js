0d36f3679f1ca2e38476e42e1390800276a1e7d9
Danny <sdanielabelvedere@gmail.com>
2017-08-04 02:05:07 -0300

    queries + hack sidenav for all devices

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   // Initialize collapse button
-  $(".button-collapse").sideNav();
+  $("#superHack").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
 
